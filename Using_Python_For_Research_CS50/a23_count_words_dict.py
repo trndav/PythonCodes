d081046f55cd7f8d91185409376bea0ed3a55def
trndav <127663307+trndav@users.noreply.github.com>
2024-06-16 20:54:04 +0200

    Update, count with collections library

--- Using_Python_For_Research_CS50/a23_count_words_dict.py
+++ Using_Python_For_Research_CS50/a23_count_words_dict.py
@@ -1,11 +1,13 @@
-# Needs improvement, to count words which end with .,!? 
-
-test = "This is test text We are keeping this text short to keep things manageable."
+text = "This is test text. We are keeping this text short to keep things manageable. !Test text!"
 
 def count_words(text):
     '''
     Count words in string and adds them to dictionary.
     '''
+    text = text.lower()
+    skip = [".", ",", "!", "?", "'", '"', ":", ";"]
+    for char in skip:
+        text = text.replace(char, "")
     word_count = {}
     for word in text.split(" "):
         # known word
@@ -16,4 +18,21 @@ def count_words(text):
             word_count[word] = 1
     return word_count 
 
-count_words(text)
+print(count_words(text))
+
+
+# With collections library
+from collections import Counter
+def count_words_with_counter(text):
+    '''
+    Count words in string and adds them to dictionary.
+    '''
+    text = text.lower()
+    skip = [".", ",", "!", "?", "'", '"', ":", ";"]
+    for char in skip:
+        text = text.replace(char, "")
+
+    word_count = Counter(text.split(" "))
+    return word_count
+
+print(count_words_with_counter(text))
