237a9ff5a10c114e57a43591af50f52d3c34258a
trndav <127663307+trndav@users.noreply.github.com>
2024-06-18 22:09:12 +0200

    Added unique word counter + sum of total words

--- Using_Python_For_Research_CS50/a23_count_words_dict.py
+++ Using_Python_For_Research_CS50/a23_count_words_dict.py
@@ -36,3 +36,35 @@ def count_words_with_counter(text):
     return word_count
 
 print(count_words_with_counter(text))
+
+
+text = read_book("book.txt")
+
+def read_book(title_path):
+    ''' 
+    Read a book and return it as string.
+    '''
+    with open(title_path, "r", encoding="utf8") as file:
+        text = file.read()
+        text = text.replace("\n", "").replace("\r", "")
+    return text
+
+print(read_book("book.txt"))
+
+
+
+def word_stats(count_words):
+    '''
+    Return number of unique words and word frequency.
+    '''
+    num_unique = len(count_words)
+    counts = count_words.values()
+    return (num_unique, counts)
+
+text = read_book("book.txt")
+words_count = count_words(text)
+
+(num_unique, counts) = word_stats(words_count)
+
+print("Unique words: ", num_unique)
+print("Total sum words: ", sum(counts))
