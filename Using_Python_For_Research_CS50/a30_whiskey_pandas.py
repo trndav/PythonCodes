5e4c1d6a0ef7a01e65f614dd36f7989850cdae83
trndav <farsiteguide@gmail.com>
2024-07-01 17:49:45 +0200

    Whiskey classification, Series, DataFrame

---
+++ Using_Python_For_Research_CS50/a30_whiskey_pandas.py
@@ -0,0 +1,63 @@
+# Pandas serie - 1d, dataframe +2d
+import pandas as pd
+import numpy as np
+
+# x2 = pd.Series([6,3,8,6])
+# print(x2)
+
+# x3 = pd.Series([6,3,8,6], index = ["q", "w", "e", "r"])
+# print(f"{x3} \n {x3[['w', 'r', 'q']]}")
+
+# age = {"Tim": 29, "Jim": 31, "Pam": 27, "Sam": 35}
+# x4 = pd.Series(age) # Keys are sorted
+# print(x4)
+
+
+# data = {"name": ["Tim", "Jim", "Pam", "Sam"],
+#         "age": [29, 31, 27, 35],
+#         "ZIP": ["10000", "12100", "10200", "45111"]}
+# x = pd.DataFrame(data, columns = ["name", "age", "ZIP"])
+# print(x)
+# print(x["name"])
+# print(x.name)
+
+
+# y = pd.Series([6,3,8,6], index = ["q", "w", "e", "r"])
+# print(y.index)
+# print(sorted(y.index))
+# c = y.reindex(sorted(y.index)) # Reindex - sort indexes in different order
+# print(c)
+
+
+# a = pd.Series([2,3,1,4], index = ["e", "q", "r", "t"])
+# print(a + y) # Sum by indexes, e + e = 8 + 2
+
+
+
+whisky = pd.read_csv("whiskies.txt")
+whisky["Region"] = pd.read_csv("regions.txt") # Add new column to whisky variable
+print(whisky.head())
+
+# print(whisky.iloc[0:10])
+# print(whisky.iloc[6:11, 0:5]) # Show specific rows and columns
+
+# print(whisky.columns) # List names of columns
+flavors = whisky.iloc[:, 2:14] # Row columns
+print(flavors)
+
+# Explore correlation - Pearson correlation
+corr_flavors = pd.DataFrame.corr(flavors) # - correlation
+print(corr_flavors)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,10))
+plt.pcolor(corr_flavors) # -pcolor plot a correlation matrix by color
+plt.colorbar()
+plt.savefig("corr_flavors.pdf")
+
+corr_whisky = pd.DataFrame.corr(flavors.transpose()) # Transpose, revert x for y
+plt.figure(figsize=(10,10))
+plt.pcolor(corr_whisky)
+plt.colorbar()
+plt.savefig("corr_whisky.pdf")
+plt.show()
