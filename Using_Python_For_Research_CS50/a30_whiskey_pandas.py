02c303ff074e693b638b8525eba35a5df1b31362
trndav <farsiteguide@gmail.com>
2024-07-02 18:37:10 +0200

    Whisky correlation, group by similar flavors

--- Using_Python_For_Research_CS50/a30_whiskey_pandas.py
+++ Using_Python_For_Research_CS50/a30_whiskey_pandas.py
@@ -43,21 +43,56 @@ print(whisky.head())
 
 # print(whisky.columns) # List names of columns
 flavors = whisky.iloc[:, 2:14] # Row columns
-print(flavors)
+# print(flavors)
 
 # Explore correlation - Pearson correlation
 corr_flavors = pd.DataFrame.corr(flavors) # - correlation
-print(corr_flavors)
+# print(corr_flavors)
 
 import matplotlib.pyplot as plt
-plt.figure(figsize=(10,10))
-plt.pcolor(corr_flavors) # -pcolor plot a correlation matrix by color
-plt.colorbar()
-plt.savefig("corr_flavors.pdf")
+# plt.figure(figsize=(10,10))
+# plt.pcolor(corr_flavors) # -pcolor plot a correlation matrix by color
+# plt.colorbar()
+# plt.savefig("corr_flavors.pdf")
 
 corr_whisky = pd.DataFrame.corr(flavors.transpose()) # Transpose, revert x for y
-plt.figure(figsize=(10,10))
+# plt.figure(figsize=(10,10))
+# plt.pcolor(corr_whisky)
+# plt.colorbar()
+# plt.savefig("corr_whisky.pdf")
+# plt.show()
+
+
+# Spectral co-clustering, eigenvalues, eigenvectors
+from sklearn.cluster import SpectralCoclustering
+
+model = SpectralCoclustering(n_clusters = 6, random_state = 0)
+model.fit(corr_whisky)
+''' .rows_ is a convention used in scikit-learn to differentiate between the parameters that are passed 
+when creating the model and attributes that are calculated or set when the model is fitted.'''
+#print(model.rows_)
+
+print(np.sum(model.rows_, axis = 1)) # How many whiskey belong to cluster 1, 2, 3..
+print(np.sum(model.rows_, axis = 0))
+print(model.row_labels_)
+
+
+whisky["Group"] = pd.Series(model.row_labels_, index = whisky.index) # Order by group labels (spectral cocluster)
+whisky = whisky.iloc[np.argsort(model.row_labels_)] # Reset index of dataframe
+whisky = whisky.reset_index(drop=True) # Reshuffled columns and rows
+
+correlations = pd.DataFrame.corr(whisky.iloc[:,2:14].transpose()) # Correlation on all rows 2:14 columns
+correlations = np.array(correlations)
+
+plt.figure(figsize = (14, 4))
+plt.subplot(121)
 plt.pcolor(corr_whisky)
-plt.colorbar()
-plt.savefig("corr_whisky.pdf")
+plt.title("Original")
+plt.axis("tight")
+plt.subplot(122)
+plt.pcolor(correlations)
+plt.title("Rearranged")
+plt.axis("tight")
+plt.savefig("correlations.pdf")
+# Whiskey in same blocks (squares) are more similar in taste, flavor
 plt.show()
