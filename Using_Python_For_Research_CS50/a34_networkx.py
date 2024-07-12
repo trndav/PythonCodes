36b1268e860e8d3c886651cd4d464966e98ef3e1
trndav <farsiteguide@gmail.com>
2024-07-12 18:37:31 +0200

    Basics of networkx

---
+++ Using_Python_For_Research_CS50/a34_networkx.py
@@ -0,0 +1,35 @@
+import networkx as nx
+
+G = nx.Graph()
+G.add_node(1) # add single node
+
+G.add_nodes_from([2,3]) # Add multiple nodes names as list
+G.add_nodes_from(["Tim","Bob"]) 
+print("G is:", G)
+print("G.nodes is:", G.nodes())
+
+G.add_edge(1, 2)
+G.add_edge("Tim", "Bob")
+
+G.add_edges_from([(1,3), (1,4), (1,5), (1,6)])
+
+G.add_edge("A", "B")
+print("All edges G.edges()", G.edges())
+
+# Remove nodes:
+G.remove_node("Bob")
+print("All edges G.edges()", G.edges())
+
+G.remove_nodes_from([4, 5])
+print("All edges G.edges()", G.edges())
+
+#Remove edges
+G.remove_edge(1,3)
+print(G.edges())
+
+# Remove multiple edges
+G.remove_edges_from([(1,2), (1,6)])
+print("G.edges():", G.edges())
+
+print(G.number_of_nodes())
+print(G.number_of_edges())
