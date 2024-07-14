aa1d7861b4366b10d0a11778682c6ced997a3663
trndav <farsiteguide@gmail.com>
2024-07-14 10:17:19 +0200

    Random graph Erdős-Rényi

--- Using_Python_For_Research_CS50/a34_networkx.py
+++ Using_Python_For_Research_CS50/a34_networkx.py
@@ -43,14 +43,55 @@ import networkx as nx
 # Random graph generators
 # Dataset karate club graph
 
-G = nx.karate_club_graph()
+# G = nx.karate_club_graph()
 import matplotlib.pyplot as plt
 
-nx.draw(G, with_labels=True, node_color="lightblue", edge_color="gray")
+# nx.draw(G, with_labels=True, node_color="lightblue", edge_color="gray")
 #plt.savefig("karate_club_graph.pdf")
 #plt.show()
 
 # Degrees of nodes
-print(G.degree())
-print(G.degree()[33])
-print(G.degree(0) is G.degree()[0])
+# print(G.degree())
+# print(G.degree()[33])
+# print(G.degree(0) is G.degree()[0])
+
+
+# Random graph Erdős-Rényi
+# build ER model
+from scipy.stats import bernoulli
+# bernoulli.rvs(p=0.2) #p-probability
+
+# N = 20
+# p = 0.2
+# # create empty graph
+# # add all N nodes in graph
+# # loop over all pairs of nodes
+#     # add an edge with probability p
+# G = nx.Graph()
+# G.add_nodes_from(range(N))
+# for node1 in G.nodes():
+#     for node2 in G.nodes():
+#         if node1 < node2 and bernoulli.rvs(p=p): # == True:
+#             G.add_edge(node1, node2)
+
+# print(G.number_of_nodes())
+
+# # Draw above graph
+# nx.draw(G)
+# plt.show()
+
+# Turn to a function
+def er_graph(N, p):
+    '''Generate ER graph.
+    '''
+    G = nx.Graph()
+    G.add_nodes_from(range(N))
+    for node1 in G.nodes():
+        for node2 in G.nodes():
+            if node1 < node2 and bernoulli.rvs(p=p): # == True:
+                G.add_edge(node1, node2)
+    return G
+
+nx.draw(er_graph(10, 0), node_size=30, node_color="gray")
+plt.savefig("erdos_renyi_graph.pdf")
+plt.show()
