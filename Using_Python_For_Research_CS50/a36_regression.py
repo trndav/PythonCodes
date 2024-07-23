f7980733437b6ca110ce1e9d57eecad6f2c399af
trndav <farsiteguide@gmail.com>
2024-07-23 17:34:32 +0200

    scikit-learn Linear Regression

--- Using_Python_For_Research_CS50/a36_regression.py
+++ Using_Python_For_Research_CS50/a36_regression.py
@@ -27,7 +27,7 @@ def compute_rss(y_estimate, y):
 def estimate_y(x, b_0, b_1):
   return b_0 + b_1 * x
 rss = compute_rss(estimate_y(x, beta_0, beta_1), y)
-print(rss)
+#print(rss)
 
 # Least Squares Estimation in Code
 
@@ -39,7 +39,7 @@ for slope in slopes:
 
 ind_min = np.argmin(rss)
 # print(ind_min)
-print("Estimate of slope: ", slopes[ind_min])
+#print("Estimate of slope: ", slopes[ind_min])
 
 # plt.figure()
 # plt.plot(slopes, rss)
@@ -56,4 +56,47 @@ print(est.summary())
 X = sm.add_constant(x)
 mod = sm.OLS(y, X)
 est = mod.fit()
-print(est.summary())
+#print(est.summary())
+
+
+# Multiple Linear Regression
+# scikit-learn for Linear Regression
+
+n = 500
+beta_0 = 5
+beta_1 = 2
+beta_2 = -1
+np.random.seed(1)
+x_1 = 10 * ss.uniform.rvs(size=n)
+x_2 = 10 * ss.uniform.rvs(size=n)
+y = beta_0 + beta_1 * x_1 + beta_2 * x_2 + ss.norm.rvs(loc = 0, scale = 1, size = n)
+
+X = np.stack([x_1, x_2], axis = 1)
+
+from mpl_toolkits.mplot3d import Axes3D 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0], X[:,1], y, c=y)
+ax.set_xlabel("$x_1$")
+ax.set_ylabel("$x_2$")
+ax.set_zlabel("$y$")
+#plt.show()
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+# Ako trebate normalizirati podatke, koristite StandardScaler
+model = make_pipeline(StandardScaler(), LinearRegression())
+model.fit(X, y)
+
+intercept = model.named_steps['linearregression'].intercept_
+coef = model.named_steps['linearregression'].coef_
+
+# lm  = LinearRegression(fit_intercept = True)
+# lm.fit(X, y)
+# LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
+print(intercept)
+print(coef[0])
+print(coef[1])
