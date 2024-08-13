5f9a45d0137805c4984b094bfb0530040efcf7aa
trndav <farsiteguide@gmail.com>
2024-08-13 10:55:46 +0200

    Length of Last Word without split

--- Practices/a15_last_word_length.py
+++ Practices/a15_last_word_length.py
@@ -19,9 +19,19 @@ class Solution:
         # return len(s[-1])
         
         # Method 2
-        if s.split():
-            return len(s.split()[-1])
-        return 0
+        # if s.split():
+        #     return len(s.split()[-1])
+        # return 0
+    
+        # Method 3 without split
+        count = 0
+        for letter in s[::-1]: #[::-1] reversed order
+            if letter == " ":
+                if count >= 1:
+                    return count
+            else:
+                count += 1
+        return count  
 
 x = Solution()
 print(x.lengthOfLastWord("Hello World"))
