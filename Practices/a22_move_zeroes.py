9236c412b3c59cdc6a3c8da9294081f44724f7d5
trndav <farsiteguide@gmail.com>
2024-08-28 18:41:45 +0200

    Move zeroes 2

--- Practices/a22_move_zeroes.py
+++ Practices/a22_move_zeroes.py
@@ -21,11 +21,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for num in nums:
-            if num == 0:
-                nums.remove(num)
-                nums.append(num)
-        return nums
+
+        # Method 1
+        # for num in nums:
+        #     if num == 0:
+        #         nums.remove(num)
+        #         nums.append(num)
+        # return nums
+
+        # Method 2
+
+        zero_count = nums.count(0)
+        next_non_zero = 0
+        for n in nums:
+            if n != 0:
+                nums[next_non_zero] = n 
+                next_non_zero += 1
+        for zero in range(1, zero_count + 1):
+            nums[-zero] = 0
+            
+
 
 x = Solution()
 print(x.moveZeroes([0,1,0,3,12])) 
