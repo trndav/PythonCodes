aa380a4ae6138a2c944a4df65e229a43674b7e04
trndav <farsiteguide@gmail.com>
2024-10-03 21:18:46 +0200

    Search Insert Position

---
+++ Practices/a28_insert_search_position.py
@@ -0,0 +1,34 @@
+# Search Insert Position
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+# If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+# Example 2:
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+
+class Solution:
+    def searchInsert(self, nums: [int], target: int) -> int:
+
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        # If target is not found, return the position where it should be inserted
+        return left
+
+x = Solution()
+print(x.searchInsert([1,3,5,6], 6))
