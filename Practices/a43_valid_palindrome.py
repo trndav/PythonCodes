7bd941c7726ad8bdefb14663c8c779b5f84a8ace
trndav <farsiteguide@gmail.com>
2024-10-18 19:39:12 +0200

    Valind palindrome

---
+++ Practices/a43_valid_palindrome.py
@@ -0,0 +1,35 @@
+# Valid Palindrome
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+# it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# Example 1:
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+# Example 2:
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+# Example 3:
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # remove non alphanumeric and all lowercase
+        a = ''.join(char for char in s if char.isalnum()).lower()
+        reversed = a[::-1]
+        
+        if a == reversed:
+            return True 
+        else:
+            return False
+
+x = Solution()
+print(x.isPalindrome("A man, a plan, a canal: Panama"))
