63434d24af033fcac05296d5e979acdbf5f05de1
trndav <farsiteguide@gmail.com>
2024-10-24 20:25:12 +0200

    Excel Sheet Column Number

---
+++ Practices/a49_excel_sheet_number.py
@@ -0,0 +1,38 @@
+# Excel Sheet Column Number
+# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+# For example:
+# A -> 1
+# B -> 2
+# C -> 3
+# ...
+# Z -> 26
+# AA -> 27
+# AB -> 28 
+# ...
+ 
+
+# Example 1:
+# Input: columnTitle = "A"
+# Output: 1
+
+# Example 2:
+# Input: columnTitle = "AB"
+# Output: 28
+
+# Example 3:
+# Input: columnTitle = "ZY"
+# Output: 701
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for char in columnTitle:
+            # Convert the current character to a number (A=1, B=2, ..., Z=26)
+            num = ord(char) - ord('A') + 1
+            # Update result by shifting the previous value left (multiply by 26)
+            result = result * 26 + num
+        return result
+
+x = Solution()
+print(x.titleToNumber("AA"))
