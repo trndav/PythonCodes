83e63a4c103198a2da346a1285c6ddf94411623e
trndav <farsiteguide@gmail.com>
2024-10-31 20:51:20 +0100

    Swap Nodes in Pairs

---
+++ Practices/a56_swap_nodes.py
@@ -0,0 +1,48 @@
+# Swap Nodes in Pairs
+# Given a linked list, swap every two adjacent nodes and return its head. 
+# You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+# Example 1:
+# Input: head = [1,2,3,4]
+# Output: [2,1,4,3]
+
+# Example 2:
+# Input: head = []
+# Output: []
+
+# Example 3:
+# Input: head = [1]
+# Output: [1]
+
+# Example 4:
+# Input: head = [1,2,3]
+# Output: [2,1,3]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def swapPairs(self, head: [ListNode]) -> [ListNode]:
+                # Dummy node to simplify the process of swapping head nodes
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        
+        # Traverse the list in pairs
+        while current.next and current.next.next:
+            # Identify the nodes in the pair
+            first = current.next
+            second = current.next.next
+            
+            # Swap the nodes
+            first.next = second.next
+            second.next = first
+            current.next = second
+            
+            # Move to the next pair
+            current = first
+        
+        # Return the new head of the modified list
+        return dummy.next
