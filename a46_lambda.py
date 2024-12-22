9bed711bf19839544451dd50a3982048115e2ee6
trndav <farsiteguide@gmail.com>
2024-12-22 20:38:36 +0100

    Lambda

---
+++ a46_lambda.py
@@ -0,0 +1,22 @@
+# Lambda
+
+double = lambda x: x*2
+print(double(5))
+
+add = lambda a, b: a + b 
+print(add(5, 3))
+
+is_even = lambda x: x % 2 == 0
+print(is_even(3))
+print(is_even(6))
+
+pairs = [(1, 3), (4, 1), (2, 2)]
+pairs.sort(key=lambda x: x[1])
+print(pairs)
+
+counter = lambda x: len(x)
+print(counter("text"))
+
+numbers = [1, 6, 3, 8, 4, 10]
+filte = list(filter(lambda x: x > 5, numbers))
+print(filte)
