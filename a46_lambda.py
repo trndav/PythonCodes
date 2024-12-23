f55cfe269ecb0def867753987a2a4f46bd4e3e6a
trndav <farsiteguide@gmail.com>
2024-12-23 21:57:06 +0100

    Lambda 2

--- a46_lambda.py
+++ a46_lambda.py
@@ -20,3 +20,18 @@ print(counter("text"))
 numbers = [1, 6, 3, 8, 4, 10]
 filte = list(filter(lambda x: x > 5, numbers))
 print(filte)
+
+list1 = [1, 2, 3, 4, 5]
+square = map(lambda x: x**2, list1)
+print(list(square))
+
+combine = lambda x, y: x + " " + y
+print(combine("Hello", "World"))
+
+words = ["banana", "apple", "cherry", "date"]
+last_char_sort = sorted(words, key=lambda x: x[-1])
+print(last_char_sort)
+
+words = ["banana", "apple", "cherry", "date"]
+words.sort(key=lambda x: x[-1])
+print(words)
