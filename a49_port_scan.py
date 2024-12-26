ee8fcdcf60065c58d181987040469bd4bfcafa4b
trndav <farsiteguide@gmail.com>
2024-12-26 21:58:59 +0100

    Port scanner

---
+++ a49_port_scan.py
@@ -0,0 +1,20 @@
+# Port scan
+
+import socket
+
+def scan_port(target_host, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((target_host, port))
+        if result == 0:
+            print(f"Port {port}: Open")
+        sock.close()
+    except:
+        pass
+
+target_host = "192.168.1.1"
+port_range = range(1, 100)
+
+for port in port_range:
+    scan_port(target_host, port)
