792d4cce9a935c1e072dc30e3c822feec13b31cc
trndav <farsiteguide@gmail.com>
2025-10-15 17:24:20 +0200

    Testing palindrome code

--- a00_tests.py
+++ a00_tests.py
@@ -1,7 +1,19 @@
-size = 5
-fill_ratio = 0.30
+# size = 5
+# fill_ratio = 0.30
 
-def generate_board(size, fill_ratio):
-    board = [['.' for _ in range(size)] for _ in range(size)]
-    return board
-print(generate_board(size, fill_ratio))
+# def generate_board(size, fill_ratio):
+#     board = [['.' for _ in range(size)] for _ in range(size)]
+#     return board
+# print(generate_board(size, fill_ratio))
+
+
+def is_palindrome(x):
+    x = x.replace(" ", "").lower()
+    b = x[::-1].lower().replace(" ", "")
+    if x == b:
+        print(True)
+
+is_palindrome("Never odd or even") #→ True
+
+#a = "bom"
+# print(a[::-1])
